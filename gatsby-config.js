089a3205cb46be3d13a73b02297ec5d47b5adc4a
theremotecreative <jason@theremotecreative.com>
2021-11-10 17:20:11 -0600

    new source

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://chronicpa.theremotecreative.com/graphql`,
+          `http://admin.chronicpa.com/graphql`,
       },
     },
 
