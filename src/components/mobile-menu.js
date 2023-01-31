bab3e76c275b90e0808849733f89d70c4f727ec5
theremotecreative <jason@theremotecreative.com>
2023-01-31 09:33:16 -0600

    foot pain menu update

--- src/components/mobile-menu.js
+++ src/components/mobile-menu.js
@@ -84,7 +84,7 @@ class MobileMenu extends Component {
                                 {/* <li><Link to={"/spinal-pain/"}>Spinal Pain</Link></li> */}
                                 <li><Link to={"/hip-pain-chiropractor/"}>Hip Pain</Link></li>
                                 <li><Link to={"/knee-pain/"}>Knee Pain</Link></li>
-                                <li><Link to={"/foot-pain-chiropractor/"}>Foot Pain</Link></li>
+                                <li><Link to={"/foot-pain-relief/"}>Foot Pain</Link></li>
                                 <li><Link to={"/car-accident-pain/"}>Car Accident Pain</Link></li>
                                 <li><Link to={"/numbness-tingling/"}>Numbness/Tingling</Link></li>
                                 <li><Link to={"/chiropractic-for-herniated-discs/"}>Herniated Disc</Link></li>
