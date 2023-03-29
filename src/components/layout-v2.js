d429890916ea0eeec7b9cd4c3405c47037d36b37
Andru <andru@tinymighty.com>
2023-03-29 15:29:42 +0100

    Remove font import; moved to gatsby-browser

--- src/components/layout-v2.js
+++ src/components/layout-v2.js
@@ -3,10 +3,6 @@ import PropTypes from "prop-types"
 import styled from 'styled-components'
 //import PageTransition from 'gatsby-plugin-page-transitions'
 
-import "@fontsource/roboto"
-import "@fontsource/roboto/500.css"
-import "@fontsource/roboto-slab"
-
 import "../css/slick.css"
 import "../css/slick-theme.css"
 import "../css/image-gallery.scss"
